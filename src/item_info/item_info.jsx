56e99cd61160586ddbd57e7f520aecf4e3977f50
rice <rhysmartinsen@gmail.com>
2025-02-24 06:59:32 -0700

    Extra Final Fix I had to add with shirts not desplaying info

--- src/item_info/item_info.jsx
+++ src/item_info/item_info.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export function Item_Info() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>Item_Info displayed here</div>
+    <main>
+        <h1>Item Info</h1>
+        <span><img src={"/ShirtDemo.png"} width="200" alt="Shirt" classNameName="img-fluid" /><p>$45 | 5/100 Bids</p></span>
+        <p>This item is made of ------</p>
+        <p>It was made in america</p>
+        <p>Very Cheap no cost</p>
+        <h3><button>Bid</button></h3>
     </main>
   );
 }
