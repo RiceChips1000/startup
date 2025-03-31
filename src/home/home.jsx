bf8ad9956a7ee52b7e9b4281deb977789e5dceda
rice1000 <rhysmartinsen@gmail.com>
2025-03-30 20:29:50 -0600

    Added functionality to home page and cart

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -8,12 +8,9 @@ export function Home() {
 
     <h1 className="specific-page-title">Top Bids</h1>
     
-    <main>
-        <div>
-            <div className="bid-item"><NavLink to="/item_info"><img src={"/ShirtDemo.png"} width="200" alt="Shirt" classNameName="img-fluid" /><p>$35 | 34/100 Bids</p></NavLink></div>
-          <div className="bid-item"><NavLink to="/item_info"><img src={"/ShirtDemo.png"} width="200" alt="Shirt" classNameName="img-fluid" /><p>$35 | 34/100 Bids</p></NavLink></div>
-        </div>
-      </main>
+    
+        <Viewbid/>
+      
 
       </>
   );
