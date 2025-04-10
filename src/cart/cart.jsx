24011f4b19514366f2b099fb86c33e85bec64a28
rice1000 <rhysmartinsen@gmail.com>
2025-04-10 02:42:51 -0600

    Added more to cart and more features will be added to cart once I get my datbase set up

--- src/cart/cart.jsx
+++ src/cart/cart.jsx
@@ -20,14 +20,17 @@ export function Cart() {
     <>
       <h1 className="specific-page-title">Your Cart</h1>
 
+
+
+    //This code will work once I get the data base set up correctly cause the item info I made based off the url and this would be to silly so yeah
       <main>
         {cartItems.length > 0 ? (
           cartItems.map((item, index) => (
             <div key={index}>
               <div className="bid-item">
-            <NavLink to={`/item_info/${index}`} className="text-light text-decoration-none">
+                <NavLink to={`/item_info/${index}`} className="text-light text-decoration-none">
                   <img src={item.image || "/ShirtDemo.png"} width="200" alt={item.name} className="img-fluid" />
-                  <p className="mt-2">${item.cost} | {item.bids}/{item.bidsNeeded} Bids</p>
+                  <p className="mt-2">{item.name} | ${item.cost} | {item.bids}/{item.bidsNeeded} Bids</p>
                 </NavLink>
               </div>
               <p>
