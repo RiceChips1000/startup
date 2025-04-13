e8c05b314bd549bf81c994d24384ecbfdccad216
rice1000 <rhysmartinsen@gmail.com>
2025-04-13 11:33:44 -0600

    Index.js file starting

--- service/index.js
+++ service/index.js
@@ -1,3 +1,14 @@
+const express = require('express');
+const app = express();
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.send('Does this work?');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
