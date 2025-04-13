83000c4a6c354f420649ac71e043a44db9842b80
rice1000 <rhysmartinsen@gmail.com>
2025-04-13 12:30:19 -0600

    Index js file save

--- service/index.js
+++ service/index.js
@@ -2,12 +2,25 @@ const express = require('express');
 const app = express();
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
+app.use(express.json()); 
 app.use(express.static('public')); 
 
+let listings = [];
+let userCarts = {};
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html'); /
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+// My Api Routes
+
+// Get all listings
+app.get('/api/listings', (req, res) => {
+  res.json(listings);
+});
+
+// Add a new listing
+app.post('/api/listings', (req, res) => {
+  listings.push(req.body);
+  res.status(201).json({ message: "Listing added" });
 });
