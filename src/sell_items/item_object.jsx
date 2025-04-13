64692ba864f42f841303a41eef632bf0c726c5b1
rice1000 <rhysmartinsen@gmail.com>
2025-04-13 14:24:45 -0600

    Adding in the rest of the inputs to flush it out

--- src/sell_items/item_object.jsx
+++ src/sell_items/item_object.jsx
@@ -36,7 +36,11 @@ export function ListingItemInfo() {
     <form onSubmit={handleSubmit}>
       <h1>List Item</h1>
       <input type="text" name="name" value={values.name} onChange={handleChanges} placeholder="Product Name" required />
-      
+      <input type="text" name="cost" value={values.cost} onChange={handleChanges} placeholder="Cost" required />
+      <input type="text" name="bidsNeeded" value={values.bidsNeeded} onChange={handleChanges} placeholder="Bids Needed" required />
+      <textarea name="about" value={values.about} onChange={handleChanges} placeholder="About Product" required />
+      <input type="file" name="image" onChange={handleChanges} />
+      <button type="submit">Submit</button>
     </form>
   );
 }
