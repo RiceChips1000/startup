161d83ee92160f31a34db8c30f299a2176fb243d
rice1000 <rhysmartinsen@gmail.com>
2025-04-17 00:12:02 -0600

    changed to 3000 port

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,10 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:4000',
-        changeOrigin: true,
-      },
+      '/api': 'http://localhost:3000',
       '/ws': {
-        target: process.env.VITE_WS_URL || 'ws://localhost:4000',
+        target: 'ws://localhost:3000',
         ws: true,
-        changeOrigin: true,
       },
     },
   },
