3dbd3114f59283432d0b8df41b33ff9fecf591f6
rice1000 <rhysmartinsen@gmail.com>
2025-04-16 22:42:08 -0600

    added sell items with lastest listing

--- src/sell_items/sell_items.jsx
+++ src/sell_items/sell_items.jsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import {ListingItemInfo} from './item_object'
+import { ListingItemInfo } from './item_object';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-// <div className="bid-item"><NavLink to="/item_info"><img src={"/ShirtDemo.png"} width="200" alt="Shirt" classNameName="img-fluid" /><p>$35 | 34/100 Bids</p></NavLink></div>
-//<img src={"/ShirtDemo.png"} width="200" alt="Shirt" classNameName="img-fluid" />
-export function Sell_Items() {
+export function Sell_Items({ setLatestListing }) {
   return (
     <>
-
-    <h1 className="specific-page-title">Your Listings</h1>
-
-    <main>
-      
-      
-
-      <ListingItemInfo />
-
-    </main>
+      <h1 className="specific-page-title">Your Listings</h1>
+      <main>
+        <ListingItemInfo setLatestListing={setLatestListing} />
+      </main>
     </>
   );
 }
